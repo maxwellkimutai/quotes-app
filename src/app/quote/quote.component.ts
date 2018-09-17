6ee107c0221ce4724c56825f20446e100a6207b7
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-17 18:07:54 +0300

    add quote object and adding a quote function

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+    quotes = [
+        new Quote(1,'I think being in love with life is a key to eternal youth', 'Maxwell','Tom', new Date(),0,0,0,false),
+        new Quote(2, 'You are only here for a short visit. Do not hurry, don not worry. And be sure to smell the flowers along the way', 'Maxwell','Tom',new Date(),0,0,0,false),
+        new Quote(3, 'A man who dares to waste one hour of time has not discovered the value of life', 'Maxwell','Tom',new Date(),0,0,0,false),
+        new Quote(4, 'If life were predictable it would cease to be life, and be without flavor', 'Maxwell','Tom',new Date(),0,0,0,false),
+        new Quote(5, 'All life is an experiment. The more experiments you make the better', 'Maxwell','Tom',new Date(),0,0,0,false)
+
+    ]
+
+
+    get addQuoteFunc() {
+        return this.addQuote.bind(this);
+    }
+
+    addQuote(author:string,quote:string,submittee:string) {
+        let number = this.quotes.length + 1;
+        let date = new Date();
+        this.quotes.push(new Quote(number,quote,author,submittee,date,0,0,0,false));
+    }
+
+    
+    }
+
+  constructor() {
+      for(let quote of this.quotes) {
+          var addTime =function () {
+              let today; //get current date and time
+              setInterval(()=>{(today = new Date()).getTime()},1000);
+              setInterval(()=>{dateDifferenceSeconds()},1000);
+              var dateDifferenceSeconds = function () {
+                  var timeDifference:number = (today - quote.dateCreated.getTime())/1000;
+                  quote.duration = timeDifference;
+              }
+          }
+          addTime();
+      }
+  }
+
+  ngOnInit() {
+  }
+
+}
